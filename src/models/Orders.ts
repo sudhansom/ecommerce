a85ba373728f12e64a1c18f9a455d5eb7c1bea19
bhuwan <bkspoudel@gmail.com>
2022-01-30 18:19:57 +0100

    added Orders model

---
+++ src/models/Orders.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document } from 'mongoose'
+import Users, { UserDocument } from './Users'
+import orderItemSchema, { OrderItemDocument } from './OrderItem'
+
+export type OrdersDocument = Document & {
+  totalPrice: number
+  users: UserDocument[]
+  orderItem: OrderItemDocument
+  completed: boolean
+}
+const ordersSchema = new mongoose.Schema({
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Users',
+    },
+  ],
+  orderItem: '',
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+export default mongoose.model<OrdersDocument>('Orders', ordersSchema)
