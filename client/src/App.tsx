7cb33e67ca6dcfc06ee3b4e54190e90a8c9cf30f
bhuwan <bkspoudel@gmail.com>
2022-01-31 18:42:50 +0100

    installed axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,13 @@ import GoogleLogin from 'react-google-login'
 
 function App() {
   const responseGoogle = (response: any) => {
-  console.log(response);
+  console.log(response.tokenId);
 }
   return (
     <div className="App">
        <GoogleLogin
     clientId="1055268114660-v6c1ohs1dc1h9nnqicoglnire4sse33l.apps.googleusercontent.com"
+    // {process.env.REACT_APP_GOOGLE_CLIENT_ID as string}
     buttonText="Login"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
