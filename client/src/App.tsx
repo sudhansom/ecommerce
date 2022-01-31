a4620a1a664ee896b2aa45ca7a057d58cb24c96b
bhuwan <bkspoudel@gmail.com>
2022-01-31 13:04:01 +0100

    added react-google-login package in app.ts in frontend and added Googlelogin component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import GoogleLogin from 'react-google-login'
+
 
 function App() {
   return (
     <div className="App">
-      <p>New project</p>
+       <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
     </div>
   );
 }
