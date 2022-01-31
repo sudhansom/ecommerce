6d89f1bbd25042b53effd7a06bb8e777d57eb1d0
bhuwan <bkspoudel@gmail.com>
2022-01-31 12:23:01 +0100

    created a googleStrategy inside passport.ts

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,11 @@
 import passport from 'passport'
 import passportLocal from 'passport-local'
-
+import GoogleTokenStrategy from 'passport-google-id-token'
+import { Strategy } from 'passport-jwt'
 import { Request, Response, NextFunction } from 'express'
 
-const LocalStrategy = passportLocal.Strategy
+//const LocalStrategy = passportLocal.Strategy
+
+const googleStrategy = new GoogleTokenStrategy({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+})
