920c077d05c303acc2842d01ef21aa9a50a370f4
bhuwan <bkspoudel@gmail.com>
2022-02-01 17:40:09 +0100

    added axios interceptors for request in index.ts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+axios.interceptors.request.use(request => {
+  const requestHeader = {
+    Authorization: `Bearer ${localStorage.getItem("token")}` as string
+  }
+  request.headers = requestHeader
+  return request
+})
 
 ReactDOM.render(
   <React.StrictMode>
